ca29a93dd4d178cb2a805908497e297bbc38c206
Christian Sanchez <143117105+csanchez38@users.noreply.github.com>
2024-10-15 14:49:29 -0600

    Update ClassAssign5.py

--- ClassAssign5.py
+++ ClassAssign5.py
@@ -29,10 +29,4 @@ x_axis = st.selectbox('Select column for X-axis', columns)
 y_axis = st.selectbox('Select column for Y-axis', columns)
 
 # Choose chart type: Bar Chart or Line Chart
-chart_type = st.selectbox('Select chart type', ['Bar Chart', 'Line Chart'])
-
-# Display chart based on user selections
-if chart_type == 'Bar Chart':
-    st.bar_chart(df[[x_axis, y_axis]].set_index(x_axis))
-elif chart_type == 'Line Chart':
-    st.line_chart(df[[x_axis, y_axis]].set_index(x_axis))
+chart_type = st
